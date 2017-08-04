593ec892c6ffce50e8bf1976944b812eacdeeccf
sudheergodugu <sudheergodugu@gmail.com>
2017-08-04 13:44:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one', function (req, res) {
+  res.send('article two requested and served here');
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
