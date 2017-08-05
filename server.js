4d495fe2646b158433621fb5485aa9875355a9ef
sudheergodugu <sudheergodugu@gmail.com>
2017-08-05 10:36:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
   
-  app.get('/home', function (req, res) {
+  app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html1'));
   
   
